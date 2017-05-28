512ee92f449fb04ea86ae4536c592874e45fb73b
CHA0S14 <chaoscatorce14@gmail.com>
2017-05-28 17:04:15 +0200

    Ahora carga automaticamente los datos en el form de modificacion

--- src/main/resources/static/js/adminUsers.js
+++ src/main/resources/static/js/adminUsers.js
@@ -8,3 +8,17 @@ $(document).on('click',".btnEliminar",function (e) {
 $(document).on('click',"#eliminar",function () {
     $("#eliminarForm" + idUsuario).submit();
 });
+
+$(document).on('click',".btnModificar",function (e) {
+    idUsuario = e.target.id;
+    //reseteaFormularios();
+
+    $("#idForm").val(idUsuario);
+    $("#usrForm").val( $("#usuario" + idUsuario).text().split(":")[1]);
+    $("#emailForm").val($("#email" + idUsuario).text().split(":")[1]);
+    var valor = 1;
+    if ($("#tipo" + idUsuario).text().split(":")[1] === "Administrador"){
+        valor = 2;
+    }
+    $("#tipoForm").val(valor);
+});

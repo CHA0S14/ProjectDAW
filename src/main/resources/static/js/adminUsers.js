a85d5e3a813d458751e22a934232330ab3fdc9bd
CHA0S14 <chaoscatorce14@gmail.com>
2017-05-28 17:34:31 +0200

    Ahora carga automaticamente los datos en el form de modificacion

--- src/main/resources/static/js/adminUsers.js
+++ src/main/resources/static/js/adminUsers.js
@@ -1,6 +1,10 @@
 /**
  * Created by Fran on 24/05/2017.
  */
+$(document).ready(function () {
+    reseteaFormularios();
+});
+
 var idUsuario = "";
 $(document).on('click',".btnEliminar",function (e) {
     idUsuario = e.target.id;
@@ -22,3 +26,65 @@ $(document).on('click',".btnModificar",function (e) {
     }
     $("#tipoForm").val(valor);
 });
+
+var emailPattern= /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+$(document).on('click',"#crearUsuario",function (e) {
+    e.preventDefault();
+    var vale = true;
+    if($("#usernameForm").val()===""){
+        $("#errorName").show();
+        $("#divName").addClass("has-error");
+        vale = false;
+    }else{
+        $("#errorName").hide();
+        $("#divName").removeClass("has-error");
+    }
+    if($("#emailFormCreate").val() === "" || !$("#emailFormCreate").val().match(emailPattern)){
+        $("#errorMail").show();
+        $("#divMail").addClass("has-error");
+        vale = false;
+    }else{
+        $("#errorMail").hide();
+        $("#divMail").removeClass("has-error");
+    }
+    if($("#emailFormCreate2").val() === "" || $("#emailFormCreate").val() !== $("#emailFormCreate2").val()){
+        $("#errorMail2").show();
+        $("#divMail2").addClass("has-error");
+        vale = false;
+    }else{
+        $("#errorMail2").hide();
+        $("#divMail2").removeClass("has-error");
+    }
+    if($("#passForm").val() === ""){
+        $("#errorPass").show();
+        $("#divPass").addClass("has-error");
+        vale = false;
+    }else{
+        $("#errorPass").hide();
+        $("#divPass").removeClass("has-error");
+    }
+    if($("#passForm2").val() === "" || $("#passForm").val() !== $("#passForm2").val()){
+        $("#errorPass2").show();
+        $("#divPass2").addClass("has-error");
+        vale = false;
+    }else{
+        $("#errorPass2").hide();
+        $("#divPass").removeClass("has-error");
+    }
+    if(vale){
+        $("#createUserForm").submit();
+    }
+});
+
+function reseteaFormularios(){
+    $("#errorName").hide();
+    $("#divName").removeClass("has-error");
+    $("#errorMail").hide();
+    $("#divMail").removeClass("has-error");
+    $("#errorMail2").hide();
+    $("#divMail2").removeClass("has-error");
+    $("#errorPass").hide();
+    $("#divPass").removeClass("has-error");
+    $("#errorPass2").hide();
+    $("#divPass2").removeClass("has-error");
+}
